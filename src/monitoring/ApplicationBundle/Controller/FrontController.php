90339145cb8855c6a7f4298f9e33fba0c018e45a
gauvin <gauvin.thibaut83@gmail.com>
2015-03-29 22:10:02 +0200

    Ajout d'un template bootstrap pour le front-end du site FrontController etc...

---
+++ src/monitoring/ApplicationBundle/Controller/FrontController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace monitoring\ApplicationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+
+class FrontController extends Controller
+{
+
+    public function homeAction()
+    {
+        return $this->render('ApplicationBundle:Front:index.html.twig');
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('ApplicationBundle:Front:about.html.twig');
+    }
+
+    public function downloadAction()
+    {
+        return $this->render('ApplicationBundle:Front:download.html.twig');
+    }
+
+    public function apidocAction()
+    {
+        return $this->render('ApplicationBundle:Front:apidoc.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('ApplicationBundle:Front:contact.html.twig');
+    }
+
+}
